b68a329fa9bfda051ab5efdd599338013fb8ef2a
Pdavis34 <paytondavis40@gmail.com>
2022-06-22 16:59:25 -0400

    implemented Auth0

--- mathtagon/src/app/app.module.ts
+++ mathtagon/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AuthModule } from '@auth0/auth0-angular';
+import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './user/auth/auth.component';
+import { NavBarComponent } from './common/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    NavBarComponent
   ],
   imports: [
+    AuthModule.forRoot({
+      domain: environment.authDomain,
+      clientId: environment.authClientId
+    }),
     BrowserModule,
     AppRoutingModule
   ],
---
+++ mathtagon/src/app/common/nav-bar/nav-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Component({
+  selector: 'nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  constructor(private auth: AuthService) { }
+
+  user: any = {}
+
+  ngOnInit(): void {
+    this.auth.user$.subscribe(u => {
+      this.user = u;
+    })
+  }
+
+}
