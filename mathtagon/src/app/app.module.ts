cfa34462c2b706ef921e1b53be6c9d149d6dc06f
Ryan Jacobo <ryanjacobo@yahoo.com>
2022-06-22 13:54:16 -0700

    added navbar

--- mathtagon/src/app/app.module.ts
+++ mathtagon/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { NavBarComponent } from './common/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
